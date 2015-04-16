3fcd96c660d5673b8a1ef921af9cb5c9658cd718
huangfeilong <huangfeilong@zhiping.tv>
2015-04-16 16:26:14 +0800

    rename project

--- heartbeat/webapps/monitor/cron/task.py
+++ heartbeat/webapps/monitor/cron/task.py
@@ -14,7 +14,7 @@ PROJ_DIR = SCRIPT_DIR.replace('/webapps/monitor/cron', '')
 
 sys.path.append(PROJ_DIR)
 sys.path.append(os.path.join(PROJ_DIR, 'webapps'))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supervisor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heartbeat.settings')
 
 from monitor.views import *
 
